d0abce54a1275d1ab09ffa7238d944dfdbf216c6
qersha <a.secgeler.imp@gmail.com>
2023-03-16 10:28:05 +0300

    Reading a TXT File

--- Dosya_Islemleri/Form1.Designer.cs
+++ Dosya_Islemleri/Form1.Designer.cs
@@ -35,6 +35,14 @@
             this.buttonSelectFile = new System.Windows.Forms.Button();
             this.buttonSaveFile = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileDirectoryTXT = new System.Windows.Forms.TextBox();
+            this.fileNameTXT = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonReadTXT = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // folderBrowserDialog1
@@ -48,6 +56,7 @@
             this.buttonLocationSelect.FlatAppearance.BorderSize = 5;
             this.buttonLocationSelect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonLocationSelect.Font = new System.Drawing.Font("rainyhearts", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonLocationSelect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
             this.buttonLocationSelect.Location = new System.Drawing.Point(902, 12);
             this.buttonLocationSelect.Name = "buttonLocationSelect";
             this.buttonLocationSelect.Size = new System.Drawing.Size(121, 53);
@@ -60,7 +69,8 @@
             // 
             this.locationTXT.AutoSize = true;
             this.locationTXT.Font = new System.Drawing.Font("rainyhearts", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.locationTXT.Location = new System.Drawing.Point(532, 317);
+            this.locationTXT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.locationTXT.Location = new System.Drawing.Point(130, 317);
             this.locationTXT.Name = "locationTXT";
             this.locationTXT.Size = new System.Drawing.Size(65, 20);
             this.locationTXT.TabIndex = 1;
@@ -78,6 +88,7 @@
             this.buttonSelectFile.FlatAppearance.BorderSize = 5;
             this.buttonSelectFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonSelectFile.Font = new System.Drawing.Font("rainyhearts", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSelectFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
             this.buttonSelectFile.Location = new System.Drawing.Point(902, 88);
             this.buttonSelectFile.Name = "buttonSelectFile";
             this.buttonSelectFile.Size = new System.Drawing.Size(121, 60);
@@ -93,13 +104,110 @@
             this.buttonSaveFile.FlatAppearance.BorderSize = 5;
             this.buttonSaveFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonSaveFile.Font = new System.Drawing.Font("rainyhearts", 16F, System.Drawing.FontStyle.Bold);
+            this.buttonSaveFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
             this.buttonSaveFile.Location = new System.Drawing.Point(902, 175);
             this.buttonSaveFile.Name = "buttonSaveFile";
-            this.buttonSaveFile.Size = new System.Drawing.Size(121, 97);
+            this.buttonSaveFile.Size = new System.Drawing.Size(121, 60);
             this.buttonSaveFile.TabIndex = 3;
             this.buttonSaveFile.Text = "Save File";
             this.buttonSaveFile.UseVisualStyleBackColor = false;
             this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("rainyhearts", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Directory:";
+            // 
+            // fileDirectoryTXT
+            // 
+            this.fileDirectoryTXT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.fileDirectoryTXT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.fileDirectoryTXT.Location = new System.Drawing.Point(182, 30);
+            this.fileDirectoryTXT.Name = "fileDirectoryTXT";
+            this.fileDirectoryTXT.Size = new System.Drawing.Size(193, 20);
+            this.fileDirectoryTXT.TabIndex = 5;
+            // 
+            // fileNameTXT
+            // 
+            this.fileNameTXT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.fileNameTXT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.fileNameTXT.Location = new System.Drawing.Point(182, 110);
+            this.fileNameTXT.Name = "fileNameTXT";
+            this.fileNameTXT.Size = new System.Drawing.Size(193, 20);
+            this.fileNameTXT.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("rainyhearts", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.label2.Location = new System.Drawing.Point(59, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File Name:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.button2.FlatAppearance.BorderSize = 5;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("rainyhearts", 16F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.button2.Location = new System.Drawing.Point(182, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Select Directory";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("rainyhearts", 16F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.button1.Location = new System.Drawing.Point(182, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 27);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Create File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonReadTXT
+            // 
+            this.buttonReadTXT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.buttonReadTXT.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(110)))), ((int)(((byte)(129)))));
+            this.buttonReadTXT.FlatAppearance.BorderSize = 5;
+            this.buttonReadTXT.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonReadTXT.Font = new System.Drawing.Font("rainyhearts", 16F, System.Drawing.FontStyle.Bold);
+            this.buttonReadTXT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(219)))), ((int)(((byte)(187)))));
+            this.buttonReadTXT.Location = new System.Drawing.Point(182, 276);
+            this.buttonReadTXT.Name = "buttonReadTXT";
+            this.buttonReadTXT.Size = new System.Drawing.Size(193, 27);
+            this.buttonReadTXT.TabIndex = 11;
+            this.buttonReadTXT.Text = "Read A TXT";
+            this.buttonReadTXT.UseVisualStyleBackColor = false;
+            this.buttonReadTXT.Click += new System.EventHandler(this.buttonReadTXT_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 175);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 108);
+            this.listBox1.TabIndex = 12;
             // 
             // Form1
             // 
@@ -107,6 +215,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(56)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1055, 463);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonReadTXT);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.fileNameTXT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileDirectoryTXT);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonSaveFile);
             this.Controls.Add(this.buttonSelectFile);
             this.Controls.Add(this.locationTXT);
@@ -127,6 +243,14 @@
         private System.Windows.Forms.Button buttonSelectFile;
         private System.Windows.Forms.Button buttonSaveFile;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileDirectoryTXT;
+        private System.Windows.Forms.TextBox fileNameTXT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonReadTXT;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Dosya_Islemleri/Form1.cs
+++ Dosya_Islemleri/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Dosya_Islemleri
 {
     public partial class Form1 : Form
@@ -42,6 +42,40 @@ namespace Dosya_Islemleri
         private void buttonSaveFile_Click(object sender, EventArgs e)
         {
             saveFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileDir= folderBrowserDialog1.SelectedPath;
+                fileDirectoryTXT.Text = fileDir;
+            }
+        }
+
+        string fileDir, fileName;
+
+        private void buttonReadTXT_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(openFileDialog1.FileName);
+                string line = read.ReadLine();
+
+                while(line != null)
+                {
+                    listBox1.Items.Add(line);
+                    line = read.ReadLine();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileName = fileNameTXT.Text;
+            string path = fileDir + "\\" + fileName + ".txt"; 
+            StreamWriter sw = File.CreateText(path);
+            MessageBox.Show("Your file has been succesfully created.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
